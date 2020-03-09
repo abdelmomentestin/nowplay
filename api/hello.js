2166cd9cbb53ee99ca00e992812b3b295020bfeb
hfireb808 <60431740+hfireb808@users.noreply.github.com>
2020-03-09 13:21:53 +0200

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,5 +1,14 @@
 module.exports = (req, res) => {
-  res.json({
-    body: "Hello NOW FUNC !"
-  })
+    const axios = require('axios');
+    axios.get('https://hn.algolia.com/api/v1/search?query=Stephen+Hawking')
+    .then(function (results) {
+        // handle success
+        console.log(results);
+        return res.json(results)
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(error, error.message);
+    });
+  
 }
